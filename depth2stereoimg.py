feae259b14a636f4078dc8a79c08731ee4d61ad9
Lezcool <tonyembers@outlook.com>
2023-10-19 02:32:59 +0200

    init

---
+++ depth2stereoimg.py
@@ -0,0 +1,314 @@
+"""make variations of input image"""
+
+import argparse, os
+import PIL
+import torch
+import numpy as np
+from omegaconf import OmegaConf
+from PIL import Image
+from tqdm import tqdm, trange
+from itertools import islice
+from einops import rearrange, repeat
+from torchvision.utils import make_grid
+from torch import autocast
+from contextlib import nullcontext
+from pytorch_lightning import seed_everything
+from imwatermark import WatermarkEncoder
+from stereoutils import *
+import torchvision.utils as vutils
+import sys
+
+sys.path.append('./stablediffusion')
+sys.path.append('./DPT')
+from ldm.util import instantiate_from_config
+from dpt.models import DPTDepthModel
+from ldm.data.util import AddMiDaS
+
+torch.set_grad_enabled(False)
+
+def chunk(it, size):
+    it = iter(it)
+    return iter(lambda: tuple(islice(it, size)), ())
+
+
+def load_model_from_config(config, ckpt, verbose=False):
+    print(f"Loading model from {ckpt}")
+    pl_sd = torch.load(ckpt, map_location="cpu")
+    if "global_step" in pl_sd:
+        print(f"Global Step: {pl_sd['global_step']}")
+    sd = pl_sd["state_dict"]
+    model = instantiate_from_config(config.model)
+    m, u = model.load_state_dict(sd, strict=False)
+    if len(m) > 0 and verbose:
+        print("missing keys:")
+        print(m)
+    if len(u) > 0 and verbose:
+        print("unexpected keys:")
+        print(u)
+
+    model.cuda()
+    model.eval()
+    return model
+
+
+def load_img(path):
+    image = Image.open(path).convert("RGB")
+    w, h = image.size
+    print(f"loaded input image of size ({w}, {h}) from {path}")
+    return image
+
+def make_batch_sd(
+        image,
+        txt,
+        device,
+        num_samples=1,
+        model_type="dpt_hybrid"
+):
+    image = np.array(image.convert("RGB"))
+    image = torch.from_numpy(image).to(dtype=torch.float32) / 127.5 - 1.0
+    # sample['jpg'] is tensor hwc in [-1, 1] at this point
+    midas_trafo = AddMiDaS(model_type=model_type)
+    batch = {
+        "jpg": image,
+        "txt": num_samples * [txt],
+    }
+    batch = midas_trafo(batch)
+    batch["jpg"] = rearrange(batch["jpg"], 'h w c -> 1 c h w')
+    batch["jpg"] = repeat(batch["jpg"].to(device=device),
+                          "1 ... -> n ...", n=num_samples)
+    batch["midas_in"] = repeat(torch.from_numpy(batch["midas_in"][None, ...]).to(
+        device=device), "1 ... -> n ...", n=num_samples)
+    return batch
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--prompt",
+        type=str,
+        nargs="?",
+        default="a painting of a virus monster playing guitar",
+        help="the prompt to render"
+    )
+
+    parser.add_argument(
+        "--init_img",
+        type=str,
+        nargs="?",
+        default="ori_img/ori-2.png",
+        help="path to the input image"
+    )
+
+    parser.add_argument(
+        "--outdir",
+        type=str,
+        nargs="?",
+        help="dir to write results to",
+        default="outputs/img2stereoimg-samples"
+    )
+
+    parser.add_argument(
+        "--ddim_steps",
+        type=int,
+        default=50,
+        help="number of ddim sampling steps",
+    )
+
+    parser.add_argument(
+        "--fixed_code",
+        action='store_true',
+        help="if enabled, uses the same starting code across all samples ",
+    )
+
+    parser.add_argument(
+        "--ddim_eta",
+        type=float,
+        default=0.0,
+        help="ddim eta (eta=0.0 corresponds to deterministic sampling",
+    )
+    parser.add_argument(
+        "--n_iter",
+        type=int,
+        default=1,
+        help="sample this often",
+    )
+
+    parser.add_argument(
+        "--C",
+        type=int,
+        default=4,
+        help="latent channels",
+    )
+    parser.add_argument(
+        "--f",
+        type=int,
+        default=8,
+        help="downsampling factor, most often 8 or 16",
+    )
+
+    parser.add_argument(
+        "--n_samples",
+        type=int,
+        default=2,
+        help="how many samples to produce for each given prompt. A.k.a batch size",
+    )
+
+    parser.add_argument(
+        "--n_rows",
+        type=int,
+        default=0,
+        help="rows in the grid (default: n_samples)",
+    )
+
+    parser.add_argument(
+        "--scale",
+        type=float,
+        default=9.0,
+        help="unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
+    )
+
+    parser.add_argument(
+        "--strength",
+        type=float,
+        default=0.8,
+        help="strength for noising/unnoising. 1.0 corresponds to full destruction of information in init image",
+    )
+
+    parser.add_argument(
+        "--from-file",
+        type=str,
+        help="if specified, load prompts from this file",
+    )
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="v2-midas-inference.yaml",
+        help="path to config which constructs model",
+    )
+    parser.add_argument(
+        "--ckpt",
+        type=str,
+        help="path to checkpoint of model",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=42,
+        help="the seed (for reproducible sampling)",
+    )
+    parser.add_argument(
+        "--direction",
+        type=str,
+        choices=["uni", "bi"],
+        default="uni"
+    )
+    parser.add_argument(
+        "--stereo_balance",
+        type=int,
+        choices=[-1,0],
+        default=0
+    )
+    parser.add_argument(
+        "--no_full_sample",
+        action='store_true',
+        default=False,
+    )
+    parser.add_argument("--depthmodel_path",type=str,required=True,help='path of depth model')
+    return parser.parse_args()
+
+def main(opt):
+    do_full_sample = False if opt.no_full_sample else True
+    
+    seed_everything(opt.seed)
+
+    config = OmegaConf.load(f"{opt.config}")
+    model = load_model_from_config(config, f"{opt.ckpt}")
+
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    model = model.to(device)
+
+    sampler = StereoShiftSampler(model, device=device)
+
+    os.makedirs(opt.outdir, exist_ok=True)
+    outpath = opt.outdir
+
+    batch_size = opt.n_samples
+    n_rows = opt.n_rows if opt.n_rows > 0 else batch_size
+
+    sample_path = os.path.join(outpath, "samples")
+    os.makedirs(sample_path, exist_ok=True)
+
+    assert os.path.isfile(opt.init_img)
+    init_image = load_img(opt.init_img) #.to(device)
+
+    # sampler.make_schedule(ddim_num_steps=opt.ddim_steps, ddim_eta=opt.ddim_eta, verbose=False)
+
+    assert 0. <= opt.strength <= 1., 'can only work with strength in [0.0, 1.0]'
+    t_enc = int(opt.strength * opt.ddim_steps)
+    print(f"target t_enc is {t_enc} steps")
+
+    sampler.make_schedule(ddim_num_steps=opt.ddim_steps, ddim_eta=opt.ddim_eta, verbose=False)
+    with torch.no_grad():
+        with model.ema_scope():
+            batch = make_batch_sd(init_image, txt=opt.prompt, device=device, num_samples=opt.n_samples)
+            z = model.get_first_stage_encoding(model.encode_first_stage(batch[model.first_stage_key]))
+            c = model.cond_stage_model.encode(batch["txt"])
+            c_cat = list()
+            for ck in model.concat_keys:
+                cc = batch[ck]
+                cc = model.depth_model(cc)
+                depth_min, depth_max = torch.amin(cc, dim=[1, 2, 3], keepdim=True), torch.amax(cc, dim=[1, 2, 3],
+                                                                                            keepdim=True)
+                display_depth = (cc - depth_min) / (depth_max - depth_min)
+                depth_image = Image.fromarray(
+                    (display_depth[0, 0, ...].cpu().numpy() * 255.).astype(np.uint8))
+                cc = torch.nn.functional.interpolate(
+                    cc,
+                    size=z.shape[2:],
+                    mode="bicubic",
+                    align_corners=False,
+                )
+                depth_min, depth_max = torch.amin(cc, dim=[1, 2, 3], keepdim=True), torch.amax(cc, dim=[1, 2, 3],
+                                                                                            keepdim=True)
+                cc = 2. * (cc - depth_min) / (depth_max - depth_min) - 1.
+                c_cat.append(cc)
+            c_cat = torch.cat(c_cat, dim=1)
+            c_cat = torch.cat([c_cat, c_cat], dim=0)
+            c = torch.cat([c, c], dim=0)
+            # cond
+            cond = {"c_concat": [c_cat], "c_crossattn": [c]}
+            
+            sa = 10
+            # prediction = depthmodel.forward(init_image)
+            editor = BNAttention(start_step=sa,direction=opt.direction)
+            regiter_attention_editor_diffusers(model, editor)   
+
+            uc_cross = model.get_unconditional_conditioning(opt.n_samples, "")
+            uc_cross = torch.cat([uc_cross, uc_cross], dim=0)
+            uc_full = {"c_concat": [c_cat], "c_crossattn": [uc_cross]}
+
+            if not do_full_sample:
+            # encode (scaled latent)
+                z_enc = sampler.stochastic_encode(
+                    z, torch.tensor([t_enc] * opt.n_samples).to(model.device))
+            else:
+                z_enc = torch.randn_like(z)
+            
+            z_enc = torch.cat([z_enc, z_enc], dim=0)
+            # decode it
+            samples = sampler.decode(z_enc, cond, t_enc, unconditional_guidance_scale=opt.scale,
+                                        unconditional_conditioning=uc_full, 
+                                        disparity=cc.squeeze(1),swapat=sa,stereo_balance=opt.stereo_balance)
+
+            x_samples = model.decode_first_stage(samples)
+            x_samples = torch.clamp((x_samples + 1.0) / 2.0, min=0.0, max=1.0)
+            output = rearrange_img(x_samples)
+            output = rearrange(output,"b c h w -> c h (b w)")
+            vutils.save_image(output, os.path.join(opt.outdir,'seed_%s.png'%opt.seed), normalize=True)
+
+   
+
+
+if __name__ == "__main__":
+    opt = parse_args()
+    main(opt)
---
+++ stereoutils.py
@@ -0,0 +1,290 @@
+import numpy as np
+import torch
+import os
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+from einops import rearrange,repeat
+
+from torchvision.utils import save_image
+import sys
+sys.path.append('./stablediffusion')
+from ldm.models.diffusion.ddim import DDIMSampler
+
+def stereo_shift_torch(input_images, depthmaps,divergence=10,stereo_balance=0.0,stereo_offset_exponent=1.0):
+    '''input: [B, C, H, W] depthmap: [B, H, W]'''
+
+    def _norm_depth(depth,max_val=1):
+        depth_min = depth.min()
+        depth_max = depth.max()
+        if depth_max - depth_min > np.finfo("float").eps:
+            out = max_val * (depth - depth_min) / (depth_max - depth_min)
+        else:
+            out = np.zeros(depth.shape, dtype=depth.dtype)
+        return out
+    
+    def _create_stereo(input_images,depthmaps,divergence,stereo_offset_exponent):
+        b, c, h, w = input_images.shape
+        derived_image = torch.zeros_like(input_images)
+        divergence_px = (divergence / 100.0) * input_images.shape[-1]
+        # filled = torch.zeros(b * h * w, dtype=torch.uint8)
+        if True:
+            for batch in range(b):
+                for row in range(h):
+                    # Swipe order should ensure that pixels that are closer overwrite
+                    # (at their destination) pixels that are less close
+                    for col in range(w) if divergence_px < 0 else range(w - 1, -1, -1):
+                        col_d = col + int((depthmaps[batch,row,col] ** stereo_offset_exponent) * divergence_px)
+                        if 0 <= col_d < w:
+                            derived_image[batch,:,row,col_d] = input_images[batch,:,row,col]
+                            # filled[batch * h * w + row * w + col_d] = 1        
+        return derived_image
+    depthmaps = _norm_depth(depthmaps)
+    balance = (stereo_balance + 1) / 2
+    if balance < 0.001:
+        left = input_images
+    else:
+        left = _create_stereo(input_images,depthmaps,+1 * divergence * balance,stereo_offset_exponent)
+    right = _create_stereo(input_images,depthmaps,-1 * divergence * (1 - balance),stereo_offset_exponent)
+    return torch.concat([left,right],axis=0)
+
+
+
+class BNAttention():
+    def __init__(self, start_step=4, total_steps=50, direction='uni'):
+
+        self.total_steps = total_steps
+        self.start_step = start_step
+        self.cur_step = 0
+        self.cur_att_layer = 0
+        self.direction = direction
+
+    
+    def attn_batch(self, q, k, v, sim, attn, is_cross, place_in_unet, num_heads, **kwargs):
+        n_samples =attn.shape[0]//num_heads // 2
+        q = rearrange(q, "(s b h) n d -> (b h) (s n) d", h=num_heads,b=n_samples)
+        k = rearrange(k, "(s b h) n d -> (b h) (s n) d", h=num_heads,b=n_samples)
+        v = rearrange(v, "(s b h) n d -> (b h) (s n) d", h=num_heads,b=n_samples)
+
+        sim = torch.einsum("h i d, h j d -> h i j", q, k) * kwargs.get("scale")
+        del q,k
+        attn = sim.softmax(-1)
+        out = torch.einsum("h i j, h j d -> h i d", attn, v)
+        out = rearrange(out, "(b h) (s n) d -> (s b) n (h d)",b=n_samples,s=2,h=num_heads)
+        return out
+    
+    def forward(self, q, k, v, sim, attn, is_cross, place_in_unet, num_heads, **kwargs):
+        if is_cross or (self.cur_step < self.start_step):
+            out = torch.einsum('b i j, b j d -> b i d', attn, v)
+            out = rearrange(out, '(b h) n d -> b n (h d)', h=num_heads)
+            return out
+        
+        n_samples =attn.shape[0]//num_heads // 4
+        qu, qc = q.chunk(2)
+        ku, kc = k.chunk(2)
+        vu, vc = v.chunk(2)
+        attnu, attnc = attn.chunk(2)
+        _num_heads = num_heads * n_samples
+        if self.direction == 'bi':
+            out_u = self.attn_batch(qu, ku, vu, sim, attnu, is_cross, place_in_unet, num_heads, **kwargs)
+            out_c = self.attn_batch(qc, kc, vc, sim, attnc, is_cross, place_in_unet, num_heads, **kwargs)
+        elif self.direction == 'uni':
+            out_u = self.attn_batch(qu, ku[:_num_heads], vu[:_num_heads], sim[:_num_heads], attnu, is_cross, place_in_unet, num_heads, **kwargs)
+            out_c = self.attn_batch(qc, kc[:_num_heads], vc[:_num_heads], sim[:_num_heads], attnc, is_cross, place_in_unet, num_heads, **kwargs)
+        out = torch.cat([out_u, out_c], dim=0)
+        return out
+
+    def __call__(self, q, k, v, sim, attn, is_cross, place_in_unet, num_heads, **kwargs):
+        out = self.forward(q, k, v, sim, attn, is_cross, place_in_unet, num_heads, **kwargs)
+        self.cur_att_layer += 1
+        self.cur_step = self.cur_att_layer//32
+        return out
+
+
+def regiter_attention_editor_diffusers(model, editor):
+    """
+    refer from [Prompt-to-Prompt]
+    """
+    def ca_forward(self, place_in_unet):
+        def forward(x, encoder_hidden_states=None, attention_mask=None, context=None, mask=None):
+            if encoder_hidden_states is not None:
+                context = encoder_hidden_states
+            if attention_mask is not None:
+                mask = attention_mask
+
+            to_out = self.to_out
+            if isinstance(to_out, nn.modules.container.ModuleList):
+                to_out = self.to_out[0]
+            else:
+                to_out = self.to_out
+
+            h = self.heads
+            q = self.to_q(x)
+            is_cross = context is not None
+            context = context if is_cross else x
+            k = self.to_k(context)
+            v = self.to_v(context)
+            q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> (b h) n d', h=h), (q, k, v))
+
+            sim = torch.einsum('b i d, b j d -> b i j', q, k) * self.scale
+
+            if mask is not None:
+                mask = rearrange(mask, 'b ... -> b (...)')
+                max_neg_value = -torch.finfo(sim.dtype).max
+                mask = repeat(mask, 'b j -> (b h) () j', h=h)
+                mask = mask[:, None, :].repeat(h, 1, 1)
+                sim.masked_fill_(~mask, max_neg_value)
+
+            attn = sim.softmax(dim=-1)
+            # the only difference
+            out = editor(
+                q, k, v, sim, attn, is_cross, place_in_unet,
+                self.heads, scale=self.scale)
+
+            return to_out(out)
+
+        return forward
+
+    def register_editor(net, count, place_in_unet):
+        for name, subnet in net.named_children():
+            # if net.__class__.__name__ == 'Attention':  # spatial Transformer layer
+            if 'Attention' in net.__class__.__name__:
+                net.forward = ca_forward(net, place_in_unet)
+                return count + 1
+            elif hasattr(net, 'children'):
+                count = register_editor(subnet, count, place_in_unet)
+        return count
+
+    cross_att_count = 0
+    try:
+        sub_nets = model.model.diffusion_model.named_children()
+    except: 
+        sub_nets = model.unet.named_children()
+    for net_name, net in sub_nets:
+        if "down" in net_name or "input" in net_name:
+            cross_att_count += register_editor(net, 0, "down")
+        elif "mid" in net_name:
+            cross_att_count += register_editor(net, 0, "mid")
+        elif "up" in net_name or "output" in net_name:
+            cross_att_count += register_editor(net, 0, "up")
+    editor.num_att_layers = cross_att_count
+
+
+def restore_attention(model):
+    def ca_forward(self, place_in_unet):
+        def forward(x, encoder_hidden_states=None, attention_mask=None, context=None, mask=None):
+
+            if encoder_hidden_states is not None:
+                context = encoder_hidden_states
+            if attention_mask is not None:
+                mask = attention_mask
+
+            to_out = self.to_out
+            if isinstance(to_out, nn.modules.container.ModuleList):
+                to_out = self.to_out[0]
+            else:
+                to_out = self.to_out
+
+            h = self.heads
+            q = self.to_q(x)
+            is_cross = context is not None
+            context = context if is_cross else x
+            k = self.to_k(context)
+            v = self.to_v(context)
+            q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> (b h) n d', h=h), (q, k, v))
+
+            sim = torch.einsum('b i d, b j d -> b i j', q, k) * self.scale
+
+            if mask is not None:
+                mask = rearrange(mask, 'b ... -> b (...)')
+                max_neg_value = -torch.finfo(sim.dtype).max
+                mask = repeat(mask, 'b j -> (b h) () j', h=h)
+                mask = mask[:, None, :].repeat(h, 1, 1)
+                sim.masked_fill_(~mask, max_neg_value)
+
+            attn = sim.softmax(dim=-1)
+            # the only difference
+            out = torch.einsum('b i j, b j d -> b i d', sim, v)
+            out = rearrange(out, '(b h) n d -> b n (h d)', h=h)
+
+            return to_out(out)
+
+        return forward
+
+    def register_editor(net, count, place_in_unet):
+        for name, subnet in net.named_children():
+            # if net.__class__.__name__ == 'Attention':  # spatial Transformer layer
+            if 'Attention' in net.__class__.__name__:
+                net.forward = ca_forward(net, place_in_unet)
+                return count + 1
+            elif hasattr(net, 'children'):
+                count = register_editor(subnet, count, place_in_unet)
+        return count
+
+    cross_att_count = 0
+    try:
+        sub_nets = model.model.diffusion_model.named_children()
+    except: 
+        sub_nets = model.unet.named_children()
+    for net_name, net in sub_nets:
+        if "down" in net_name or "input" in net_name:
+            cross_att_count += register_editor(net, 0, "down")
+        elif "mid" in net_name:
+            cross_att_count += register_editor(net, 0, "mid")
+        elif "up" in net_name or "output" in net_name:
+            cross_att_count += register_editor(net, 0, "up")
+
+
+class StereoShiftSampler(DDIMSampler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ddimstep = 0
+        self.disparity = None
+        self.swapat = 10
+
+    @torch.no_grad()
+    def sample(self, *args, **kwargs):
+        try:
+            self.disparity = kwargs['disparity']
+            self.stereo_balance = kwargs['stereo_balance']
+            self.swapat = kwargs['swapat']
+            print(self.swapat)
+        except:
+            pass
+        
+        return super().sample(*args, **kwargs)
+
+    @torch.no_grad()
+    def p_sample_ddim(self, *args, **kwargs):
+        x_prev, pred_x0 = super().p_sample_ddim(*args, **kwargs)
+        if self.disparity is not None:
+            if self.ddimstep==self.swapat:
+                x = args[0]
+                N = x.shape[0]//2
+                disparity = torch.nn.functional.interpolate(self.disparity.unsqueeze(1),size=[64,64],mode="bicubic",align_corners=False,).squeeze(1)
+                x_prev = stereo_shift_torch(x_prev[:N],disparity,stereo_balance=self.stereo_balance)
+                pred_x0 = stereo_shift_torch(pred_x0[:N],disparity,stereo_balance=self.stereo_balance)
+            self.ddimstep += 1
+        return x_prev, pred_x0
+
+    @torch.no_grad()
+    def decode(self,*args,**kwargs):
+        try:
+            self.disparity = kwargs['disparity']
+            self.stereo_balance = kwargs['stereo_balance']
+            self.swapat = kwargs['swapat']
+            del kwargs['disparity'],kwargs['stereo_balance'],kwargs['swapat']
+        except:
+            pass
+        return super().decode(*args, **kwargs)
+    
+    
+def rearrange_img(img):
+    n = img.shape[0]
+    img_li = []
+    for i in range(n//2):
+        tmp = torch.concat((img[i],img[i+n//2]),axis=2)
+        img_li.append(tmp)
+    new_img = torch.stack(img_li,0)
+    return new_img
